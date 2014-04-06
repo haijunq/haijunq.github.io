e4a4622691cd1a709c418b22e4e3c657fdf352f5
haijunq <haijunq@gmail.com>
2014-04-06 13:56:39 -0700

    add button function

--- index.js
+++ index.js
@@ -19,3 +19,42 @@ function drawTriangle(x0, y0, x1, y1, x2, y2) {
 }
 
 */
+
+"use strict";
+var count = 1;
+var basename = "Code Relay Project "; 
+var baseid = "code_relay_project_";
+
+$(document).ready(function(){
+	$("#start_new_button").on("click", addNewProject);
+	$("#initial_button").on("click", initCode);
+	$(document).on("click","a.delete",deleteTRNode);
+});
+
+
+function addNewProject() {
+	var node = makeIdDOMNode("tr", baseid + count);
+	node.innerHTML = "<li><a>" + basename + count + "</a><a class=\"delete\" href=\"\">X</a></li>";
+	$("#projectList").append(node);
+	count ++;
+}
+
+function makeIdDOMNode(tagName, id) {
+	var idNode = document.createElement(tagName);
+	idNode.setAttribute("id", id);
+	return idNode;
+}
+
+function deleteTRNode(event) {
+	event.preventDefault();
+	$(this).parent().remove();
+}
+
+function initCode() {
+	alertme();
+	$("#iframe").contents().find("html").html("This is a iframe.");
+}
+
+function alertme() {
+	alert("me");
+}
